189cdf1b08a1fc182627a2d8e1d42edd0cf22f25
Roberto Panzeri <robertopanzeri@rocketmail.com>
2018-08-29 19:05:41 +0200

    Hero styling with BEM, CSS nesting and variables, BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssimport = require('postcss-import');
+cssimport = require('postcss-import'),
+browserSync = require('browser-sync').create();
 
 gulp.task('default', function(){
     console.log("Gulp task created");
@@ -22,8 +23,15 @@ gulp.task('styles', function(){
 
 gulp.task('watch', function(){
 
+    browserSync.init ({
+        server: {
+            baseDir: "app"
+        }
+    });
+
     watch('./app/index.html', function(){
         gulp.start('html');
+        browserSync.reload();
     });
     watch('./app/assets/styles/**/*.css', function(){
         gulp.start('styles');
