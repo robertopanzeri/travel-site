6c062eabc9f99b7813f4e00f1a349391969f9e6d
Roberto Panzeri <robertopanzeri@rocketmail.com>
2018-10-11 14:45:57 +0200

    Completed automatic sprite generation

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp'),
 svgSprite = require('gulp-svg-sprite'),
-rename = require('gulp-rename');
+rename = require('gulp-rename')
+del = require('del');
 
 var config = {
     mode: {
-        css: {
+        css: { // Activate the «css» mode, files are created in a 'css' subfolder
+            sprite: "sprite.svg", // Sprite path and name
             render: {
                 css: {
                     template: './gulp/templates/sprite.css'
@@ -14,14 +16,29 @@ var config = {
     }
 }
 
-gulp.task('createSprite', function () {
+gulp.task('beginClean', function () {
+    return del(['./app/temp/sprite', './app/assets/images/sprites']);
+});
+
+gulp.task('createSprite', ['beginClean'], function () {//beginClean is a dependency, so it will be run first. When beginClean is done, then createSprite is run
     return gulp.src('./app/assets/images/icons/**/*.svg')
         .pipe(svgSprite(config))
         .pipe(gulp.dest('./app/temp/sprite/'));
 });
 
-gulp.task('copySpriteCSS', function () {
+gulp.task('copySpriteGraphic', ['createSprite'], function () {//createSprite is a dependency, so it will be run first. When createSprite is done, then copySpriteGraphic is run
+    return gulp.src('./app/temp/sprite/css/**/*.svg')
+        .pipe(gulp.dest('./app/assets/images/sprites'));
+});
+
+gulp.task('copySpriteCSS', ['createSprite'], function () {//createSprite is a dependency, so it will be run first. When createSprite is done, then copySpriteCSS is run
     return gulp.src('./app/temp/sprite/css/*.css')
         .pipe(rename('_sprite.css'))
         .pipe(gulp.dest('./app/assets/styles/modules'));
 });
+
+gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function () {//copySpriteGraphic and copySpriteCSS are dependencies, so they will be run first. When they are done, then endClean is run
+    return del('./app/temp/sprite');
+});
+
+gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
