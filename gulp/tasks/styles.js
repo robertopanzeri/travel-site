8a0b438ef69e3d378af4d14c8bd39e00a081f16c
Roberto Panzeri <robertopanzeri@rocketmail.com>
2018-10-11 18:55:43 +0200

    Completed header styling (except mobile menu)

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,12 +3,13 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssimport = require('postcss-import');
-mixins = require('postcss-mixins');
+cssimport = require('postcss-import'),
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function(){
     return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssimport, mixins, cssvars, nested, hexrgba, autoprefixer]))
     .on('error', function(errorInfo){//If a CSS error occurs, tell this task to print the error message and go ahead normally to the end
         console.log(errorInfo.toString());
         this.emit('end');
