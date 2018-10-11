027ae020eaf9b4432440878b687c66c7f266b595
Roberto Panzeri <robertopanzeri@rocketmail.com>
2018-10-11 18:56:10 +0200

    OO JS (WIP)

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,27 @@
+/*
+console.log("Hello, my name is John Doe and my favorite color is blue.");
+console.log("Hello, my name is Jane Smith and my favorite color is green.");
+
+function person(name, favColor) {
+    console.log("Hello, my name is " + name + " and my favorite color is " + favColor + ".");
+}
+
+person("John Doe", "blue");
+person("Jane Smith", "green");
+*/
+
+/*
+var john = {
+    name: "John Doe",
+    favoriteColor: "blue",
+    greet: function (name, favColor) {
+        console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
+    }
+}
+
+john.greet();
+*/
+
+function Person() { // Constructor
+
+}
