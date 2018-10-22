b9aa301ebad45ec9cba408416a6fbb15675ba750
Roberto Panzeri <robertopanzeri@rocketmail.com>
2018-10-22 19:12:31 +0200

    Installed webpack and added Js scripts handling to gulp watch

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,11 +2,6 @@ var gulp = require('gulp'),
 watch = require('gulp-watch'),
 browserSync = require('browser-sync').create();
 
-gulp.task('cssInject', ['styles'], function(){//[dependencies that are run before the cssInject task, in this case the task styles]
-    return gulp.src('./app/temp/styles/styles.css')
-        .pipe(browserSync.stream());
-});
-
 gulp.task('watch', function(){
 
     browserSync.init ({
@@ -20,8 +15,22 @@ gulp.task('watch', function(){
         //gulp.start('html');
         browserSync.reload();
     });
+
     watch('./app/assets/styles/**/*.css', function(){
         gulp.start('cssInject');
     });
     
+    watch('./app/assets/scripts/**/*.js', function(){
+        gulp.start('scriptsRefresh');   
+    });
+
+});
+
+gulp.task('cssInject', ['styles'], function(){//[dependencies that are run before the cssInject task, in this case the task styles]
+    return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function(){
+    browserSync.reload();
 });
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,15 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "./app/temp/scripts"), //generates an absolute path, needed by webpack
         filename: "App.js"
+    },
+    module: {
+        loaders: [{
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015']
+            },
+            test: /\.js$/, //apply it just to js files
+            exclude: /node_modules/ //exclude libraries, just apply webpack to my own code
+        }]
     }
-}
+};
